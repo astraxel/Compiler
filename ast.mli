763692c3033f1c66273d586338a4755e294bf493
Skeulse <33985917+Skeulse@users.noreply.github.com>
2018-01-01 18:31:16 +0100

    Update ast.mli
    Rajout de la deuxieme partie de L'ast. J'ai certains doutes pour le moment sur certaines lignes qui sont annotes de (* pas sur de moi *).

--- ast.mli
+++ ast.mli
@@ -1,5 +1,3 @@
-
-
 type binop = Equal | Not_equal | Less | Greater | Less_or_equal | Greater_or_equal | And | Or |Plus | Minus | Times | Divide | Modulo
                                                                                                                              
 type unop = Not | Minus
@@ -64,3 +62,72 @@ type decl =
    |Ddecl_struct of decl_struct
 
 type fichier = decl list
+
+(* deuxieme partie de L'AST *)
+
+type tident = ident * int 
+
+and typ = 
+   Tbool | Tint | Tunit | Tstruct | Tvec of typ * list | Tborrow
+(*  J ai remplacé i32 par int,
+ * je sais pas quoi faire et où utiliser Tstruct 
+ * Je ne sais pas comment définir T borrow 
+ *)
+ 
+and tdecl =
+   | TDdecl_fun of tdecl_fun
+   | TDdecl_struct of tdecl_struct
+
+and tstmt =
+   | TUnit of unit
+   | TSexpr of texpr
+   | TSaff of tborrow * ident * texpr (* pas sur de moi *)
+   | TSobj of tborrow * ident * (ident*texpr) list (*pas sur de moi *)
+   | TSwhile of texpr * tbloc 
+   | TSreturn of texpr option
+   | TSif of tpif (* pif = if, mais c'est réservé *)
+   
+and tpif =
+   |TAif of texpr * tbloc (* if expr then bloc *)
+   |TBif of texpr * tbloc * tbloc (* if expr then bloc else bloc *)
+   |TIif of texpr * tbloc * tpif (* if expr then bloc else pif *)
+
+and texpr =
+   |TEint of int
+   |TEbool of bool
+   |TEident of tident
+   |TEbinop of texpr * binop * texpr
+   |TEunop of unop * texpr
+   |TEattribute of texpr * ident (* pas sur de moi*)
+   |TElen of texpr
+   |TEselect of texpr * texpr
+   |TEcall of tident * texpr list
+   |TEvect of tvec   (* pas sur de moi *)
+   |TEprint of string
+   |TEbloc of tbloc
+
+and tbloc =
+   |TUbloc of tstmt list
+   |TVbloc of tstmt list * texpr
+
+and targument = tborrow * ident * typ
+
+and ttype = 
+   |TTid of ident * typ (* pas sur de moi *)
+   |TTcons of ident * typ
+   |TTesp of mut * typ
+   |typ
+
+and tfichier =  tdecl list
+
+and tdecl_struct = {
+   name : ident ;
+   attributes : (tident*typ) list;
+}
+
+and tdecl_fun = {
+   name : ident ;
+   arguments : targument list ;
+   body : tbloc ;
+   typ : typ option
+}
