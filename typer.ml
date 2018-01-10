3eec11ce962caf1d27af653f703d497aad160f4c
Skeulse <33985917+Skeulse@users.noreply.github.com>
2018-01-10 14:21:55 +0100

    Update typer.ml

--- typer.ml
+++ typer.ml
@@ -1,95 +1,65 @@
 open Ast
+        open Tast
+	
+
+	exception Lexing_error of string
+        exception Erreur_typage of (Tast.ttyp * Tast.ttyp * Ast.loc)
+        exception Erreur_types_egaux of (Tast.ttyp *Ast.loc *Tast.ttyp *Ast.loc)
+        exception Erreur_types_non_egaux of (Tast.ttyp *Ast.loc * Tast.ttyp)  
+        exception Erreur_mut of  Ast.loc
+        exception Erreur_vide of Ast.loc
+        exception Erreur_lvalue of Ast.loc
+        exception Erreur_len of (int * int *Ast.loc) 
+        exception Erreur_no_expr of  Ast.loc
+        exception Erreur_types_non_coherents of (Tast.ttyp * Ast.loc * Tast.ttyp * Ast.loc)
+        exception Erreur_non_declare of (Tast.ttyp * Ast.loc)
+        exception Erreur_borrow of Ast.loc
+        exception Erreur_struct_call of (Tast.ttyp * ident * Ast.loc)
+        exception Erreur_not_mutable of Ast.loc 
+	
+
+	
+
+
 
 module Smap = Map.Make (String)
 module Sset = Set.Make (String) 
-type Env_variables = (bool, typ) Smap.t
-type Env_fonction = (typ list , typ) Smap.t
-type Env_structures = ((ident, typ) list )Smap.t 
-type Env = (Env_variables, Env_fonction, Env_structures)
-
-let get_variables (ev, _, _)= ev
-let get_fonction (_,ef,_) =ef
-let get_structures (_,_,es)=es
-
-exception Erreur_typage of typ * typ * loc
-exception Erreur_types_egaux of typ *loc *typ * loc
-exception Erreur_types_non_egaux of typ *loc * typ  
-exception Erreur_mut of expr * loc
-exception Erreur_vide of loc
-exception Erreur_lvalue of expr loc
-exception Erreur_len of int * int *loc 
-exception Erreur_no_expr of expr * loc
-exception Erreur_types_non_coherents of typ * loc * typ * loc
-exception Erreur_non_declare of typ * loc
-exception Erreur_borrow of loc
-exception Erreur_struct_call of typ * ident * loc
-exception Erreur_not_mutable of loc
-
 
 (*TODO les hashtbl, le e.x avec l histoire de regarder si ce st dans l ident *)
 (* penser à la loc dans AST *)
 (* def la fonction check_bf *)
 (* def la fonction check_unit, Type_no_struct, type_no_borrow, check_in, regarder si c'est une permu, Saff *)
 
-let rec deref_type t = if t =Tref (m, t1) then deref_type t1 else t
+let rec deref_type t = match t with
+   |Tref (m, t1) -> deref_type t1
+   |_ -> t
+
    
 let rec type_adapte (t1, t2) =
-   match deref_type t1 with
-      |deref_type t2 -> true
+   let a = deref_type t1 in
+   let b= deref_type t2 in
+   match a with
+      |b -> true
       |_ -> false
- 
-let rec check_uni (list_ident) =
-   match list_ident with
-      |[] -> true
-      |x::r -> 
    
- 
-let rec type_no_struct env (e, i) =
-   match e with 
-      |[] -> true
-      |x::[] ->
-         let (_, xt) as xtype = type_expr env x in
-         begin match xt with
-            |Tstruct (i) -> (*TODO faire cttte partie avec le sous vec et verifie la gestion d'erreur *)
-            |_ -> raise (Error_struct_call (xt, i, snd xt))
-      |x::y::r -> 
-         let (_,xt) as xtype = type_expr env x in
-         let (_,yt) as ytype = type_expr env y in
-         begin match xt with 
-            |Tstruct (i) -> (*TODO faire cttte partie avec le sous vec et verifie la gestion d'erreur *)
-            |_ -> raise (Error_struct_call (xt, i, snd xt))
-            
-let rec type_no_borrow env (e ) =
-   match e with
-      |[] -> true
-      |x::[] -> 
-         let (_, xt) as xtype = type_expr env x in
-         begin match check_no_borrow xt with (*TODO definir la fonction check_no_borrow *)
-            |true -> true
-            |false -> raise (Error_borrow (snd x))
-         end
-      |x::y::r -> 
-         let (_,xt) as xtype = type_expr env x in
-         let (_,yt) as ytype = type_expr env y in
-         begin match check_no_borrow xt with (*TODO definir la fonction check_no_borrow *)
-            |true -> type_no_borrow env (y::r)
-            |false -> raise (Error_borrow (snd x))
 
-let rec structure_list_with_constraint li_expr type_cible = match li_expr with
+(* let rec structure_list_with_constraint li_expr type_cible = match li_expr with
    | [] -> []
-   | x::q -> let (type_pur_x, structure_x) = type_expr env x in
-       if type_cible = type_pur_x then
-           structure_x :: (type_list_with_constraint q)
-       else
-           raise (Error_typage (type_pur_x, type_cible, snd x))
+   | x::q -> 
+       let (type_pur_x, structure_x) = type_expr env x in
+       begin match type_cible with
+          |type_pur_x -> structure_x :: (type_list_with_constraint q)
+          | _ -> raise (Erreur_typage (type_pur_x, type_cible, snd x))
+       end *)
 
-let type_list li_expr = match li_expr with
-   | [] -> ([], Vec (TUnit))
+
+(*let type_list li_expr = match li_expr with
+   | [] -> ([], TEvect ([]))
    | x::q -> let (type_pur, structure_x) = type_expr x in
-       (structure_x :: (structure_list_with_constraint q type_pur), Vec (type_pur))
+       (structure_x :: (structure_list_with_constraint q type_pur), Vec (type_pur)) *)
    
       
-let rec type_bf env (e)=
+(*let rec type_bf env (e)=
    match e with 
       |[] -> true
       |x::[] ->
@@ -104,9 +74,9 @@ let rec type_bf env (e)=
          begin match check_bf xt with (* TODO definir la fonction check_bf *)
             |true -> type_bf env (y::r) 
             |false -> raise (Error_non_declare (xt, snd x))
-         end
+         end *)
 
-let rec type_arg_comparaison_list env (list_typ, list_expr) =
+ let rec type_arg_comparaison_list env (list_typ, list_expr) =
    match list_typ with 
       |[] -> true  
       | x::[]-> begin match list_expr with 
@@ -114,51 +84,51 @@ let rec type_arg_comparaison_list env (list_typ, list_expr) =
             let r =type_adapte (y, snd x) in
             begin match r with
                | true -> true (* Une valeur random pour verifier *)
-               | false -> raise (Erreur_types_non_egaux (snd x * snd (fst x) * y )) 
+               | false -> raise (Erreur_types_non_egaux ((snd x), snd (fst x) , y )) 
             end
          end
-      end 
-      | x::y::r -> begin match list_expr with 
+      | x::y::q -> begin match list_expr with 
          |w::z::o -> 
             let r = type_adapte (w, snd x) in
             begin match r with
-               | true -> 
+               | true-> 
                   let r1 = type_adapte (z, snd y) in
                   begin match r1 with
-                     |true  -> type_arg_list (y::r , z::o)
-                     |false-> raise (Erreur_types_non_egaux (snd y * snd (fst y) * z ))
+                     |true  -> type_arg_list env (y::q , z::o)
+                     |false-> raise (Erreur_types_non_egaux (snd y,snd (fst y) , z ))
                   end
-            |false-> raise (Erreur_types_non_egaux (snd x * snd (fst x) * w )) 
+            |false-> raise (Erreur_types_non_egaux (snd x , snd (fst x) ,w )) 
             end
          end
    
-let rec type_arg_list env (list_typ, list_expr ) =
+and type_arg_list env (list_typ, list_expr ) =
    match list_typ with 
-      |-> true  
+      |[]->  true
       | x::[]-> begin match list_expr with 
          |y::[] -> begin match snd x with
             | y -> true (* Une valeur random pour verifier *)
-            | _ -> raise (Erreur_types_non_egaux (snd x * snd (fst x) * y )) 
+            | _ -> raise (Erreur_types_non_egaux (snd x, snd (fst x) , y )) 
          end
       end 
       | x::y::r -> begin match list_expr with 
          |w::z::o -> begin match snd x with
             | w -> begin match snd y with
-               | z -> type_arg_list (y::r , z::o)
-               |_ -> raise (Erreur_types_non_egaux (snd y * snd (fst y) * z ))
+               | z -> type_arg_list env (y::r , z::o)
+               |_ -> raise (Erreur_types_non_egaux (snd y , snd (fst y) , z ))
             end
-            |_ -> raise (Erreur_types_non_egaux (snd x * snd (fst x) * w )) 
+            |_ -> raise (Erreur_types_non_egaux (snd x , snd (fst x) , w )) 
          end
       end
 
-let type_mut_expr env (e, loc) = match e with 
-   |Eident name -> let (m, t) = Smap.find name env in
+let rec type_mut_expr env (e, loc) = match e with 
+   (*|Eident name -> let (m, t) = Smap.find name env in
       begin match m with 
          |true -> (TEident (name), t, true)
          |false -> raise ( Error_not_mutable (snd name))
+      end *) 
 
    |Eselect (e1, e2) ->
-      let (_, e1t, b) as e1type =type_mut_expr env e1 in
+      let (_, e1t, b) as e1type = type_mut_expr env e1 in
       let (_, e2t) as e2type =type_expr env e2 in
       let t1 = deref_type e1t in
       begin match t1 with 
@@ -189,11 +159,12 @@ let type_mut_expr env (e, loc) = match e with
                   (TEunop (unop, etype), t , b)
                |_ -> raise (Erreur_typage (et, Tref, snd e))
             end
+      end
    |_ -> raise (Erreur_mut (e, snd e))
 
  
       
-let type_lvalue_expr env (e, loc ) = match e with 
+and type_lvalue_expr env (e, loc ) = match e with 
    |Eident name -> 
       let (m, t) = Smap.find name env in
       (TEident (name), t)
@@ -219,7 +190,7 @@ let type_lvalue_expr env (e, loc ) = match e with
             end
          | _-> raise ( Erreur_typage (e1t, Tvec, snd e1))
       end 
-  |Eattribute (e, i) ->
+  (*|Eattribute (e, i) ->
       let (_, et) as etype = type_lvalue_expr env e in
       let t1 = deref_type et in
       begin match t1 with 
@@ -230,29 +201,29 @@ let type_lvalue_expr env (e, loc ) = match e with
          |_ -> raise (Erreur_typage (et, Tstruct, snd e))
       end
    |Sobj (m, i, i1, s) ->
-      let a = find.hastbl (i) in(* TODO codercette hastbl *)
+      (*let a = find.hastbl (i) in(* TODO codercette hastbl *)
       begin match a.length with 
-         |s.length ->
+         |1 ->
             let r =type_arg_list env (snd s (*ici c est e *), find.hastbl (i)) in
             begin match r with 
-               |true -> (* regarder si c est bien une permutation ! *) 
+               |true -> (* regarder si c est bien une permutation  *) 
                   
             end
          |_ -> raise ( Erreur_len (s.length , a.length, snd s ))
       end
-   |_ -> (Erreur_lvalue (e, snd e))
+   |_ -> (Erreur_lvalue (e, snd e)) *) *)
    
-let type_expr env (e , loc) = match e with 
+and type_expr env (e , loc) = match e with 
    |Eint n -> (TEint n, Tint)
    |Ebool b -> (TEbool b, Tbool)
    |Eunop ( unop , e) -> 
-      | begin match unop with 
+      begin match unop with 
          |Deref -> (*verifie si ce n'est pas une lvalue car lvalue implique value *)
             let (_, et) as etype = type_expr env e in
             begin match et with
                |Tref (_, t) -> 
                   let t1 = deref_type t in
-                  ( (TEunop (unop, etype) , t1)
+                  (TEunop (unop, etype) , t1)
                |_-> raise ( Erreur_typage (et , Tref, snd e)) 
             end
          |UMinus ->
@@ -296,7 +267,7 @@ let type_expr env (e , loc) = match e with
             begin match e1t with
                |Tint -> begin match e2t with 
                   |Tint -> (TEbinop ( e1type, op , e2type ), Tint)
-                  | _-> raise ( Erreur_typage (e2t, Tint, snd e2)
+                  | _-> raise ( Erreur_typage (e2t, Tint, snd e2))
                   end
               |_ -> raise ( Erreur_typage (e1t, Tint, snd e1))
             end
@@ -304,9 +275,10 @@ let type_expr env (e , loc) = match e with
             begin match e1t with
                |Tbool -> begin match e2t with 
                   |Tbool -> (TEbinop (e1type , op, e2type ), Tbool)
-                  | -> raise ( Erreur_typage ( e2t, Tbool , snd e2))
+                  | _-> raise ( Erreur_typage ( e2t, Tbool , snd e2))
                   end
                |_ -> raise ( Erreur_typage ( e1t, Tbool, snd e1 ))
+            end
          
          | Affect -> 
             let (_, e1t, b) as e1type =type_mut_value_expr env e1 in 
@@ -319,6 +291,7 @@ let type_expr env (e , loc) = match e with
                      |true -> (TEbinop (e1type, e2type), Tunit ) 
                   end
                |false -> raise (Erreur_types_non_coherents (e1, snd e1, e2, snd e2))
+            end
       end
       
    |Elen e ->
@@ -329,13 +302,13 @@ let type_expr env (e , loc) = match e with
          |_ -> raise ( Erreur_typage ( et, Tvec , snd e))
       end      
   
-  | Evect e ->
-     let (struct, t)= type_list env e in
-     (TEvect (struct), t)
+   |Evect e ->
+     let (structure, t)= type_list env e in
+     (TEvect (structure), t)
 
-  |Eprint s -> (TEprint s , Tunit)
+   |Eprint s -> (TEprint s , Tunit)
   
-  |Ecall (i, e) -> 
+  (*|Ecall (i, e) -> 
      let a = find.hastbl (i) in (* TODO coder cette hastbl *)
      begin match a.length with
         |e.length ->
@@ -349,7 +322,8 @@ let type_expr env (e , loc) = match e with
    |Eunop (unop , e) ->
       begin match unop with 
          
-      end
+      end *)
+
    |Eselect (e1 , e2) ->
       let (_, e1t) as e1type =type_lvalue_expr env e1 in
       let (_, e2t) as e2type =type_expr env e2 in
@@ -361,7 +335,7 @@ let type_expr env (e , loc) = match e with
             end
          | _-> raise ( Erreur_typage (e1t, Tvec, snd e1))
       end 
-  |Eattribute (e, i) ->
+   |Eattribute (e, i) ->
       let (_, et) as etype = type_lvalue_expr env e in
       let t1 = deref_type et in
       begin match t1 with 
@@ -371,7 +345,7 @@ let type_expr env (e , loc) = match e with
             (TEattribute (etype, ident), t)
          |_ -> raise (Erreur_typage (et, Tstruct, snd e))
       end
-   |Sobj (m, i, i1, s) ->
+   (*|Sobj (m, i, i1, s) ->
       let a = find.hastbl (i) in(* TODO codercette hastbl *)
       begin match a.length with 
          |s.length ->
@@ -381,16 +355,41 @@ let type_expr env (e , loc) = match e with
                   
             end
          |_ -> raise ( Erreur_len (s.length , a.length, snd s ))
-      end
-  |_ -> raise (Erreur_no_expr (e, snd e)) 
+      end *)
+   |_ -> raise (Erreur_no_expr (e, snd e)) 
   
+let type_if env ( p, loc ) = match p with 
+   |Aif ( e, b) -> 
+      let (_, et ) as etype =type_expr env e in 
+      let (_, bt) as btype = type_bloc env b in 
+      begin match et with 
+         |Tbool -> (TAif (etype, btype ), bt )
+         |_-> raise ( Erreur_typage (etype, Tbool, snd e))
+      end
+   |Bif ( e, b1 , b2) ->
+      let (_, et ) as etype =type_expr env e in 
+      let (_, b1t) as b1type = type_bloc env b1 in 
+      let (_, b2t) as b2type = type_bloc env b2 in 
+      begin match et with 
+         |Tbool -> begin match b1t with 
+            |b2t -> (TBif (etype, b1type, b2type) , b1t )
+            |_ -> raise (Erreur_types_non_egaux ( b1type, snd b1, b2type, snd b2))
+         end
+         |_-> raise ( Erreur_typage (etype, Tbool, snd e))
+      end
+   |Cif (e, b, c) ->
+      let (_, et ) as etype = type_expr env e in 
+      let (_, bt) as btype = type_bloc env b in 
+      let (_, ct) as ctype = type_if env c in 
+      begin match et with 
+         |Tbool -> begin match bt with 
+            |ct -> (TCif (etype, btype, ctype), bt)
+            |_ -> raise ( Erreur_types_non_egaux (btype, snd b , ctype, snd c))
+         end
+         |_ -> raise (Erreur_typage (etype, Tbool, snd e))
+      end
 
-let type_decl_struct (i, i1) = 
-   
-
-let type decl_fun (i, arg, b, t)=
-
-and type_stmt env (s, loc ) = match s with 
+let type_stmt env (s, loc ) = match s with 
    |Sreturn e -> 
       let (_,et) as etype= type_expr env e in
       begin match e with 
@@ -411,7 +410,7 @@ and type_stmt env (s, loc ) = match s with
       let (_,st) as stype = type_if env s in
       (TSif (stype), st) 
       
-   |Saff (m, i, i1, s) ->
+   (*|Saff (m, i, i1, s) ->
       let a = find.hastbl (i) in (* TODO codercette hastbl *)
       begin match a.length with 
          |s.length ->
@@ -420,10 +419,11 @@ and type_stmt env (s, loc ) = match s with
                |true -> (* regarder si c est bien une permutation ! *)    
             end
          |_ -> raise ( Erreur_len (s.length , a.length, snd s ))
-      end
+      end *) 
               
 
-and rec type_bloc env (liste_instr, e_finale, loc) = match liste_instr with
+
+let type_bloc env (liste_instr, e_finale, loc) = match liste_instr with
    |[] ->
          ([], type_expr env (e_finale)) 
    |instr::q -> 
@@ -445,7 +445,7 @@ and rec type_bloc env (liste_instr, e_finale, loc) = match liste_instr with
                      |Tunit ->
                         let (r, rtype)  = type_bloc env (q, e_finale) in
                         (structure ::r, rtype)
-                     |_ -> raise (Error_typage (et, Tunit, snd e)
+                     |_ -> raise (Error_typage (et, Tunit, snd e))
                   end
                |_ -> raise (Error_typage ( et, Tbool, snd e))
             end
@@ -455,54 +455,21 @@ and rec type_bloc env (liste_instr, e_finale, loc) = match liste_instr with
             (structure ::r, rtype)
             
          |Sif (e)-> 
-            let (structure, st) = type_if env (p, loc)
+            let (structure, st) = type_if env (p, loc) in 
             let (r, rtype) = type_bloc env (q, e_finale) in
             (structure ::r, rtype)
             
-         |Sobj (m, x, e) -> 
+         (*|Sobj (m, x, e) -> 
             let (structure, et) as etype = type_expr env e in
             let nouvelles_variables = (Smap.add x (m, et ) (get_variables env)) in
             let nouvel_environnement = (nouvelles_variables, get_fonction env , get_structures env) in
             let (structure_bloc,bt) as btype = type_bloc nouvel_environnement (q, e_finale) in 
-            (structure :: structure_bloc, bt )
+            (structure :: structure_bloc, bt ) *)
             
-         |Saff (m, x, i, i1)-> (* TODO Saff *)
+         (*|Saff (m, x, i, i1)-> (* TODO Saff *)
             let (structure, et) as stype =type_stmt env i1 in (* a verifier + finir *)
             let (structure_bloc, bt) as btype=type_bloc nouvel_environnement (q, e_finale) in
             
-            (structure :: structure_bloc, bt)
+            (structure :: structure_bloc, bt) *)
       end
-      
-
-and type_if env ( p, loc ) = match p with 
-   |Aif ( e, b) -> 
-      let (_, et ) as etype =type_expr env e in 
-      let (_, bt) as btype = type_bloc env b in 
-      begin match et with 
-         |Tbool -> (TAif (etype, btype ), bt )
-         |_-> raise ( Erreur_typage (etype, Tbool, snd e))
-      end
-   |Bif ( e, b1 , b2) ->
-      let (_, et ) as etype =type_expr env e in 
-      let (_, b1t) as b1type = type_bloc env b1 in 
-      let (_, b2t) as b2type = type_bloc env b2 in 
-      begin match et with 
-         |Tbool -> begin match b1t with 
-            |b2t -> (TBif (etype, b1type, b2type) , b1t )
-            |_ -> raise (Erreur_types_non_egaux ( b1type, snd b1, b2type, snd b2))
-         end
-         |_-> raise ( Erreur_typage (etype, Tbool, snd e)
-      end
-   |Cif (e, b, c) ->
-      let (_, et ) as etype =type_expr env e in 
-      let (_, bt) as btype = type_bloc env b in 
-      let (_, ct) as ctype = type_if env c in 
-      begin match et with 
-         |Tbool -> begin match bt with 
-            |ct -> (TCif (etype, btype, ctype), bt)
-            |_ -> raise ( Erreur_types_non_egaux (btype, snd b , ctype, snd c))
-         end
-         |_ -> raise (Erreur_typage (etype, Tbool, snd e)
-      end
-      
-
+ 
